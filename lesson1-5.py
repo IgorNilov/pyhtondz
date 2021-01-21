54ad5cdda72cbc6d44e806ec1f4d8ca1913b4819
Spartak <igornilov@mail.ru>
2021-01-21 18:23:04 +0300

    задание 1 задача 5

---
+++ lesson1-5.py
@@ -0,0 +1,14 @@
+pribil = int(input('введите прибыль:'))
+izder = int(input('введите издержки :'))
+a = pribil - izder
+b = int()
+if pribil > izder:
+    print(a)
+    b = int(input('введите число рабочих :'))
+if a > 0:
+    c = a / b
+    print('по', c, 'на каждого рабочего')
+elif pribil < izder:
+    print('Вы работаете в убыток')
+elif pribil == izder:
+    print('У Вас нет прибыли, но и нет убытков')
