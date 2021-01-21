af8c67b599c79810a38da20349a22a165887a06d
Igor <igornilov@mail.ru>
2021-01-21 18:40:26 +0300

    1-2

--- lesson1-2.py
+++ lesson1-2.py
@@ -1,4 +1,4 @@
-time = int(input('Введите время в секундах:'))
+time = int(input('Введите время в секундах :'))
 hour = time // 3600
 minute = (time - hour * 3600) // 60
 sek = time - (hour * 3600) - (minute * 60)
