fef52eda1127b2238b33ee3fe4e2f01a67418666
Igor <igornilov@mail.ru>
2021-02-02 21:52:54 +0300

    урок 5 задание 1

---
+++ lesson5-1.py
@@ -0,0 +1,11 @@
+fail = input('имя файла :')
+f = open(fail, 'w')
+while True:
+    s = input('Введите строку : \n')
+    if s == '': break
+    f.writelines(s + '\n')
+f.close()
+f = open(fail, 'r')
+content = f.readlines()
+print(content)
+f.close()
